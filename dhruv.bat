651fd0cd340ecd1ff10e1885c78a0eef34da47f5
Kotadiya Dhruv <108579548+Dhruvkotadiya90@users.noreply.github.com>
2026-08-16 18:19:03 +0530

    Updates

---
+++ dhruv.bat
@@ -0,0 +1,3 @@
+@echo off
+
+java -cp "%~dp0src" Main %*
--- src/Main.java
+++ src/Main.java
@@ -3,42 +3,70 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) {
 
+        // No argument
+        if (args.length == 0) {
+            System.out.println("DhruvLang 0.1.0");
+            System.out.println("Usage: dhruv <file.D>");
+            System.out.println("Use 'dhruv --help' for more information.");
+            return;
+        }
+    
+        // Version
+        if (args[0].equals("--version")) {
+            System.out.println("DhruvLang 0.1.0");
+            return;
+        }
+    
+        // Help
+        if (args[0].equals("--help")) {
+            System.out.println("DhruvLang 0.1.0");
+            System.out.println();
+            System.out.println("Usage:");
+            System.out.println("  dhruv <file.D>");
+            System.out.println();
+            System.out.println("Options:");
+            System.out.println("  --version    Show DhruvLang version");
+            System.out.println("  --help       Show this help message");
+            return;
+        }
+    
+        // Program file
         try {
-
+    
             List<String> lines = Files.readAllLines(
-                Path.of("Programms","hello.D")
+                    Path.of(args[0])
             );
-
+    
             StringBuilder code = new StringBuilder();
-
+    
             for (String line : lines) {
                 code.append(line).append("\n");
             }
-
+    
             Lexer lexer = new Lexer();
-
+    
             List<Token> tokens =
-                lexer.tokenize(code.toString());
-
+                    lexer.tokenize(code.toString());
+    
             Parser parser = new Parser(tokens);
-
+    
             List<Statement> statements =
-                parser.parse();
-
+                    parser.parse();
+    
             Interpreter interpreter =
-                new Interpreter();
-
+                    new Interpreter();
+    
             for (Statement statement : statements) {
                 interpreter.execute(statement);
             }
-
+    
         } catch (Exception e) {
-
-            System.out.println("Error running DhruvLang.");
-            e.printStackTrace();
+    
+            System.out.println(
+                    "DhruvLang Error: " + e.getMessage()
+            );
         }
     }
 }
